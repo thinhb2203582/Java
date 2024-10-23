3c42d2f786c56eec8ef00b8449b24df99c8df813
Gia Thinh <trangiathinh0309@gmail.com>
2024-10-23 09:58:19 +0700

    java

---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author trang
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+//         new LoginForm().setVisible(true);
+           new MainUI().setVisible(true);
+        
+    }
+}
